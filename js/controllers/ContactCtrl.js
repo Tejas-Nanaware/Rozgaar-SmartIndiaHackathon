e7bc4dd4d142176bdd46de8a2f52d26e29170d19
Tejas-Nanaware <nanawaret@hotmail.com>
2017-02-28 15:20:35 +0530

    removed old files

--- js/controllers/ContactCtrl.js
+++
@@ -1,6 +0,0 @@
-angular
-.module('Rozgaar')
-.controller('ContactCtrl', function($scope, $compile) {
-    console.log('inside contact controller');
-    
-});
--- js/controllers/RegisterCtrl.js
+++
@@ -1,8 +0,0 @@
-angular
-.module('Rozgaar')
-.controller('RegisterCtrl', ["$scope", "$compile", function($scope, $compile) {
-    console.log('inside register controller');
-    $scope.RegisterCtrlObject = [];
-    $scope.RegisterCtrlObject.title = "Register Here";
-    console.log($scope.RegisterCtrlObject.title);
-}]);
